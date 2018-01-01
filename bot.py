17237cd14af06d6482eb396a9a2ff1f566113215
Arduqq <twop@hotmail.de>
2018-01-01 15:10:06 +0100

    Fixed multi-line comenting problems

--- bot.py
+++ bot.py
@@ -90,11 +90,10 @@ async def on_message(message):
             text = '\n'.join(("Ich brauche eine einzige Zahl als Input, Dummkopf.",
                               "`Usage: !roll [Anzahl der geworfenen D6-Würfel]`"))
             await client.send_message(message.channel, text)
-
-    '''
-    !lovecheck
-    Überprüft, ob zwei Namen sich lieben sollten oder nicht
-    '''
+    
+    # !lovecheck
+    # Überprüft, ob zwei Namen sich lieben sollten oder nicht
+    
     elif message.content.startswith('!lovecheck'):
         try:
             laufi = ("Laufi", "Daniel", "Laufamholzer", "laufi", "daniel", "laufamholzer", "holzer", "Holzer")
@@ -119,33 +118,30 @@ async def on_message(message):
             text = '\n'.join(("Falscher Input, Dumpfbacke",
                               "`Usage: !loveCheck [Person1] [Person2]`"))
             await client.send_message(message.channel, text)
-
-    '''
-    !meme
-    Postet ein zufälliges Meme aus Imgur
-    '''
+    
+    # !meme
+    # Postet ein zufälliges Meme aus Imgur
+    
     elif message.content.startswith('!meme'):
         try:
             memes = imgurclient.memes_subgallery(sort='viral', page=0, window='week')
             await client.send_message(message.channel, random.choice(memes).link)
         except ImgurClientError:
             await client.send_message(message.channel, 'Imgur-Client spinnt. :(')
-
-    '''
-    !shibe
-    Postet ein süßes Bild von einem Shiba Inus
-    '''
+    
+    # !shibe
+    # Postet ein süßes Bild von einem Shiba Inus
+    
     elif message.content.startswith('!shibe'):
         try:
             shibes = imgurclient.subreddit_gallery('shiba', sort='time', window='week', page=0)
             await client.send_message(message.channel, random.choice(shibes).link)
         except ImgurClientError:
             await client.send_message(message.channel, 'Imgur-Client spinnt. :(')
-
-    '''
-    !choose
-    Wählt ein Element aus einer Auswahl von Elementen aus
-    '''
+    
+    # !choose
+    # Wählt ein Element aus einer Auswahl von Elementen aus
+    
     elif message.content.startswith('!choose'):
         try:
             l = message.content.split()[1:]
@@ -160,11 +156,10 @@ async def on_message(message):
             text = '\n'.join(("Liste ist leer oder hier ist gerade die Hölle los",
                               "`Usage: !choose [Auswahlelement]*`"))
             await client.send_message(message.channel, text)
-
-    '''
-    !coinflip
-    Wirft eine Münze
-    '''
+    
+    # !coinflip
+    # Wirft eine Münze
+    
     elif message.content.startswith('!coinflip'):
         try:
             coin = random.randint(1,2)
@@ -176,16 +171,15 @@ async def on_message(message):
             text = '\n'.join(("Etzala a fail",
                               "`Usage: !coinflip`"))
             await client.send_message(message.channel, text)
-
-    '''
-    !quote
-    Alle möglichen Commands in Bezug auf Zitate
-        !quote add
-        Fügt Zitate in eine Textdatei, die jederzeit mit !quote aufgerufen werden
-
-        !quote id
-        Zitiert eine Nachricht aus dem Channel mithilfe der Message-ID
-    '''
+    
+    # !quote
+    # Alle möglichen Commands in Bezug auf Zitate
+    #     !quote add
+    #     Fügt Zitate in eine Textdatei, die jederzeit mit !quote aufgerufen werden
+    # 
+    #     !quote id
+    #     Zitiert eine Nachricht aus dem Channel mithilfe der Message-ID
+    
     elif message.content.startswith('!quote'):
         await client.delete_message(message)
         try:
@@ -221,7 +215,6 @@ async def on_message(message):
                 else:
                     text = '\n'.join(("Korregation!",
                               "`Usage: !quote (add [Zitat]) | id [ID]`"))
-
             else:
                 randquote = random.choice(quotes)
                 randquote = randquote.split(' ')
@@ -232,25 +225,22 @@ async def on_message(message):
             text = '\n'.join(("Korregation!",
                               "`Usage: !quote (add [Zitat] | id [ID])`"))
             await client.send_message(message.channel, text)
-
-    '''
-    !dab
-    Lösch dich.
-    '''
+    
+    # !dab
+    # Lösch dich.
+    
     elif message.content.startswith('!dab'):
         await client.send_message(message.channel, "Lösch dich.", tts=True)
-
-    '''
-    !help
-    Verlinkung zur Website für alle nötigen Informationen
-    '''
+    
+    # !help
+    # Verlinkung zur Website für alle nötigen Informationen
+    
     elif message.content.startswith('!help'):
         await client.send_message(message.channel, "Alle Informationen zu den Bot-Commands findest du auf: http://gregorius.paperplane.io/")
-
-    '''
-    !basterds
-    Postet Informationen zum kommenden Filmabend
-    '''
+    
+    # !basterds
+    # Postet Informationen zum kommenden Filmabend
+    
     elif message.content.startswith('!basterds'):
         embed=discord.Embed(title="Victorurious Basterds", description="🎥 Der Filmabend findet jeden **Sonntag** um **20 Uhr** statt. Jeder bringt einen Film mit und wir würfeln einige Minuten vorher aus welchen davon wir uns anschauen werden, suchen einen passenden Stream heraus und lassen den Sonntag in Liebe ausklingen.", color=0xb14e38)
         embed.set_thumbnail(url="https://vignette.wikia.nocookie.net/clubpenguin/images/8/85/Popcorn.png/revision/latest/scale-to-width-down/363?cb=20161030050058")
